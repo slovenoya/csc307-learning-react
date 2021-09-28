e63ceb98703b5e363e38131fa8c56394db8d592f
BJ Klingenberg <bklingen@calpoly.edu>
2021-09-28 15:08:02 -0700

    add reddit api fetch

--- react-assignment1/src/MyApp.js
+++ react-assignment1/src/MyApp.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Table from "./Table";
 import Form from "./Form";
+import FetchDemo from "./FetchDemo";
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
@@ -20,6 +21,8 @@ function MyApp() {
     <div className="container">
       <Table characterData={characters} removeCharacter={removeOneCharacter} />
       <Form handleSubmit={updateList} />
+
+      <FetchDemo subreddit="calpoly" />
     </div>
   );
 }
